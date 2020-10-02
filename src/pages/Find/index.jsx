22b40d2d4c44ad8d4f82f1b71b2d4918e95a810a
caiofelixreis <caiofelix.reis@gmail.com>
2020-10-02 08:17:05 -0300

    find

--- src/pages/Find/index.jsx
+++ src/pages/Find/index.jsx
@@ -1,12 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Layout from '../../components/Layout'
 import style from "./style.module.scss";
-import { FiCornerUpLeft } from 'react-icons/fi'
+import { FiUser } from 'react-icons/fi'
 import { Link } from 'react-router-dom'
 import Back from '../../components/Back'
 
 
 const Find = () => {
+
+    const [clientData, setClientData] = useState([])
+
+    const handleInputSubmit = (e) => {
+        if (e.key == 'Enter') {
+            setClientData(['Caio Félix', '18 anos', 'Masculino'])
+        }
+    }
+
     return (
         <Layout back>
             <main className={style.container}>
@@ -14,9 +23,23 @@ const Find = () => {
                     <h1>Digite o código do cliente:</h1>
                 </div>
                 <div className={style.input}>
-                    <input type="text" />
+                    <input type="text" onKeyDown={handleInputSubmit} />
                     <div></div>
                 </div>
+
+                <div className={style.info}>
+                    {
+                        clientData.length &&
+                        clientData.map( (e) => (
+                       
+                            <div key={e} className={style.box}>
+                                {e}
+                            </div>
+
+                        ))
+                    }
+                </div>
+
             </main>
         </Layout>
     )
