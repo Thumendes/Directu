6811b7da0b4bc869a95025870b98c057c50de9bc
Thumendes <62642098+Thumendes@users.noreply.github.com>
2020-11-24 09:47:17 -0300

    Confirmando inserção no banco de dados

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import style from "./style.module.scss";
 import logo from "../../assets/images/logo.png";
 import { useHistory } from "react-router-dom";
@@ -6,11 +6,33 @@ import api from "../../services/api";
 import InputMask from "react-input-mask";
 import DropzoneInput from "../../components/Form/Dropzone";
 import { useState } from "react";
+import Axios from "axios";
 
 const SignUp = () => {
-  const [form, setForm] = useState({});
+  const [form, setForm] = useState({
+    name: "",
+    cnpj: "",
+    cep: "",
+    city: "",
+    road: "",
+    state: "",
+    number: "",
+    phone: "",
+    email: "",
+    password: "",
+  });
+  const [ufs, setUfs] = useState([]);
   const [image, setImage] = useState(null);
   const history = useHistory();
+
+  useEffect(() => {
+    (async () => {
+      const { data } = await Axios.get(
+        "https://servicodados.ibge.gov.br/api/v1/localidades/estados"
+      );
+      setUfs(data.map((uf) => uf.sigla));
+    })();
+  }, []);
 
   const handleSubmit = async () => {
     try {
@@ -25,9 +47,6 @@ const SignUp = () => {
       }
 
       const response = await api.post("/store", data);
-      console.log(image);
-
-      console.log(response);
 
       history.push("/auth");
     } catch (error) {
@@ -38,6 +57,21 @@ const SignUp = () => {
   const handleChangeInput = ({ target }) => {
     const { value, name } = target;
     setForm({ ...form, [name]: value });
+  };
+
+  const handleCalculateCep = async ({ target }) => {
+    const { value } = target;
+
+    const cep = value.replace(/[^\d]+/g, "");
+
+    const { data } = await Axios.get(`https://viacep.com.br/ws/${cep}/json/`);
+
+    setForm({
+      ...form,
+      road: data.logradouro,
+      state: data.uf,
+      city: data.localidade,
+    });
   };
 
   return (
@@ -58,11 +92,20 @@ const SignUp = () => {
                   placeholder="Nome"
                   type="text"
                   name="name"
+                  value={form.name}
+                  onChange={handleChangeInput}
+                />
+                <input
+                  placeholder="E-mail"
+                  type="email"
+                  name="email"
+                  value={form.email}
                   onChange={handleChangeInput}
                 />
                 <InputMask
                   mask="99.999.999/9999-99"
                   onChange={handleChangeInput}
+                  value={form.cnpj}
                 >
                   {(inputProps) => (
                     <input {...inputProps} placeholder="CNPJ" name="cnpj" />
@@ -78,14 +121,27 @@ const SignUp = () => {
             </header>
             <div className={style.form}>
               <div>
-                <InputMask mask="99.999-999" onChange={handleChangeInput}>
+                <InputMask
+                  mask="99999-999"
+                  value={form.cep}
+                  onChange={handleChangeInput}
+                  onBlur={handleCalculateCep}
+                >
                   {(inputProps) => (
                     <input {...inputProps} placeholder="CEP" name="cep" />
                   )}
                 </InputMask>
                 <input
+                  placeholder="Cidade"
+                  type="text"
+                  value={form.city}
+                  name="city"
+                  onChange={handleChangeInput}
+                />
+                <input
                   placeholder="Rua"
                   type="text"
+                  value={form.road}
                   name="road"
                   onChange={handleChangeInput}
                 />
@@ -94,12 +150,20 @@ const SignUp = () => {
                 <input
                   type="text"
                   placeholder="Estado"
+                  value={form.state}
                   name="state"
+                  list="UFs"
                   onChange={handleChangeInput}
                 />
+                <datalist id="UFs">
+                  {ufs.map((uf) => (
+                    <option value={uf} />
+                  ))}
+                </datalist>
                 <input
                   type="text"
                   placeholder="Número"
+                  value={form.number}
                   name="number"
                   onChange={handleChangeInput}
                 />
@@ -111,7 +175,11 @@ const SignUp = () => {
               <span>Conta</span>
             </header>
             <div className={style.form}>
-              <InputMask mask="(99) 99999-9999" onChange={handleChangeInput}>
+              <InputMask
+                mask="(99) 99999-9999"
+                value={form.phone}
+                onChange={handleChangeInput}
+              >
                 {(inputProps) => (
                   <input {...inputProps} placeholder="Telefone" name="phone" />
                 )}
@@ -119,6 +187,7 @@ const SignUp = () => {
               <input
                 placeholder="Senha"
                 type="password"
+                value={form.password}
                 name="password"
                 onChange={handleChangeInput}
               />
